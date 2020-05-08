a7a4d52dc532e2228e1a137dae420d4f7863b68b
Joshua Kennedy <josh@outthinkgroup.com>
2020-05-08 11:49:15 -0400

    added a gaurd to fn

--- functions/take-screenshot.js
+++ functions/take-screenshot.js
@@ -1,9 +1,11 @@
 const chromium = require("chrome-aws-lambda");
 const puppeteer = require("puppeteer-core");
 exports.handler = async (event, context) => {
-  const body = JSON.parse(event.body) || {
-    body: { url: "https://github.com/joshatoutthink/" },
-  };
+  const body = event.body
+    ? JSON.parse(event.body)
+    : {
+        body: { url: "https://github.com/joshatoutthink/" },
+      };
   const { url } = body;
 
   const browser = await puppeteer.launch({
