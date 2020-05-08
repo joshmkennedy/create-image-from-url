0090460534a7d8d938c696da24ff7bf5c8f2d198
Joshua Kennedy <josh@outthinkgroup.com>
2020-05-08 11:35:57 -0400

    removed dev-take-screenshot function so it can build

--- functions/dev-take-screenshot.js
+++ functions/dev-take-screenshot.js
@@ -1,13 +1,13 @@
-const puppeteer = require("puppeteer");
+//const puppeteer = require("puppeteer");
 
 exports.handler = async (event, context) => {
-  const browser = await puppeteer.launch({ headless: true });
+  /* const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto("https://google.com", { waitUntil: "networkidle2" });
-  const screenshot = await page.screenshot({ encoding: "binary" });
+  const screenshot = await page.screenshot({ encoding: "binary" }); */
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ screenshot }),
+    body: JSON.stringify({ screenshot: null }),
   };
 };
